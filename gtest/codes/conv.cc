ba9dd918efb21e030d7d80d5822bfe6d210f7c23
yanghaozhi <yanghaozhi@gmail.com>
2024-08-04 14:57:00 +0800

    fix code.conv

--- gtest/codes/conv.cc
+++ gtest/codes/conv.cc
@@ -10,10 +10,12 @@ SET     3   3   321.12
 SET     4   4   abc
 # watch ：1/2/3/4
 TRAP    0   4   0
-CONV    4   3   1
-CONV    5   1   3
-CONV    6   2   1
+
+CONV    5   3   1
+CONV    6   1   3
+CONV    7   2   1
 TRAP    1   6   0
+
 STORE   1   4   1
 # $1.from != NULL, $1.ref == 2
 # $abc.reg == 1, $abc.ref == 2
@@ -36,16 +38,16 @@ TEST(code, conv)
             CHECK_REG(key, 0, 3, N, TYPE_DOUBLE,   321.12, -1, 0);
             CHECK_REG(key, 0, 4, N, TYPE_STRING,   "abc",  -1, 0);
                                                       
-            CHECK_REG(key, 1, 1, N, TYPE_DOUBLE,   123.0,  -1, 0);
-            CHECK_REG(key, 1, 2, N, TYPE_SIGNED,   321,    -1, 0);
-            CHECK_REG(key, 1, 3, N, TYPE_SIGNED,   321,    -1, 0);
-            CHECK_REG(key, 1, 4, N, TYPE_SIGNED,   321,    -1, 0);
-            CHECK_REG(key, 1, 5, N, TYPE_DOUBLE,   123.0,  -1, 0);
-            CHECK_REG(key, 1, 6, N, TYPE_SIGNED,   321,    -1, 0);
+            CHECK_REG(key, 1, 1, N, TYPE_SIGNED,   123,    -1, 0);
+            CHECK_REG(key, 1, 2, N, TYPE_UNSIGNED, 321,    -1, 0);
+            CHECK_REG(key, 1, 3, N, TYPE_DOUBLE,   321.12, -1, 0);
+            CHECK_REG(key, 1, 5, N, TYPE_SIGNED,   321,    -1, 0);
+            CHECK_REG(key, 1, 6, N, TYPE_DOUBLE,   123.0,  -1, 0);
+            CHECK_REG(key, 1, 7, N, TYPE_SIGNED,   321,    -1, 0);
                                                    
-            CHECK_REG(key, 2, 1, Y, TYPE_DOUBLE,   123.0,  2, 0);
+            CHECK_REG(key, 2, 1, Y, TYPE_SIGNED,   123,  2, 0);
                                                    
-            CHECK_REG(key, 3, 1, N, TYPE_SIGNED,   123,    -1, 0);
+            CHECK_REG(key, 3, 1, N, TYPE_SIGNED,   123,  -1, 0);
 
             return match;
         },
@@ -53,9 +55,9 @@ TEST(code, conv)
         {
             int match = 0;
 
-            CHECK_VAR(key, 2, "abc", 0, 0, 1,  TYPE_DOUBLE, 123.0, 2, 0);
+            CHECK_VAR(key, 2, "abc", 0, 0, 1,  TYPE_SIGNED, 123, 2, 0);
                                                            
-            CHECK_VAR(key, 3, "abc", 0, 0, -1, TYPE_DOUBLE, 123.0, 1, 0);
+            CHECK_VAR(key, 3, "abc", 0, 0, -1, TYPE_SIGNED, 123, 1, 0);
             return match;
         });
     ASSERT_EQ(0, t.go(txt));
