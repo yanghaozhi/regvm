8ae343ff4cd586667cba10caff77764004291ab7
yanghaozhi <yanghaozhi@gmail.com>
2024-09-05 12:43:04 +0800

    完善测试脚本

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 FILE=${1:-pi}
+LOG=./t.log
 
 cd ../src/
 
@@ -9,29 +10,45 @@ make release
 
 cd -
 
-declare -A scripts=(["lua"]="lua" ["python3"]="py" ["qjs"]="js" ["duk"]="js" ["node"]="js")
-for k in "${!scripts[@]}"
-do
-    which $k > /dev/null
-    if [ $? -eq 0 ] && [ -f ${FILE}.${scripts[$k]} ]
-    then
-        echo
-        echo
-        echo testing $k ${FILE}.${scripts[$k]} ...
-        time $k ${FILE}.${scripts[$k]}
-    fi
-done
-
+declare -A results=()
 
 declare -A vm=(["vasm"]="vasm" ["vcc"]="c--")
 for k in "${!vm[@]}"
 do
-    if [ -f ${FILE}.${vm[$k]} ]
+    if [ -f ${FILE}.${vm[${k}]} ]
     then
         echo
         echo
-        echo testing $k ${FILE}.${vm[$k]} ...
-        time ../out/$k ${FILE}.${vm[$k]} -r
+        echo testing ${k} ${FILE}.${vm[${k}]} ...
+        (time ../out/${k} ${FILE}.${vm[${k}]} -r) &> ${LOG}
+        t=$(cat ${LOG} | grep real | awk '{print $2}' | awk -F[ms] '{print $1 * 60 + $2}')
+        results[${k}]=${t}
+        cat ${LOG}
+        rm -rf ${LOG}
     fi
 done
+
+declare -A scripts=(["lua"]="lua" ["python3"]="py" ["qjs"]="js" ["duk"]="js" ["node"]="js")
+for k in "${!scripts[@]}"
+do
+    which ${k} > /dev/null
+    if [ $? -eq 0 ] && [ -f ${FILE}.${scripts[${k}]} ]
+    then
+        echo
+        echo
+        echo testing ${k} ${FILE}.${scripts[${k}]} ...
+        (time ${k} ${FILE}.${scripts[${k}]}) &> ${LOG}
+        t=$(cat ${LOG} | grep real | awk '{print $2}' | awk -F[ms] '{print $1 * 60 + $2}')
+        cat ${LOG}
+        rm -rf ${LOG}
+
+        echo compare time result :
+        for r in "${!results[@]}"
+        do
+            v=$(echo "scale=3; ${t} / ${results[${r}]}" | bc -l)
+            echo "${r} : ${t} / ${results[${r}]} =" $(printf "%0.2f\n" ${v})
+        done
+    fi
+done
+
 
