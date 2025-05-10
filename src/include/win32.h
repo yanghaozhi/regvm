3349e2cfd55b3f50ff813b17317706a94809643d
yanghaozhi <yanghaozhi@gmail.com>
2025-05-10 11:41:20 +0800

    bug fix

--- src/include/win32.h
+++ src/include/win32.h
@@ -10,6 +10,9 @@
 #if defined(__clang__)
 #define likely(x)       __builtin_expect(!!(x), 1)
 #define unlikely(x)     __builtin_expect(!!(x), 0)
+#else
+#define likely(x)       (x)
+#define unlikely(x)     (x)
 #endif
 
 
