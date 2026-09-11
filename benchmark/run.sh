6a2589763b37461043ad6bb30c8a391373f46050
yanghaozhi <yanghaozhi@gmail.com>
2026-09-11 14:45:20 +0800

    支持lto选项

--- benchmark/run.sh
+++ benchmark/run.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-FILE=${1:-pi}
+if [ $# -eq 0 ]
+then
+    echo Usage : $(basename $0) [compile mode] \| [project]
+    exit 0
+fi
+
+#FILE=${2:-pi}
 LOG=./t.log
 
 cd ../src/
 
 make clean
-make release
+make $1
+
+shift 1
 
 cd -
 
